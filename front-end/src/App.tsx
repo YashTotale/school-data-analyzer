980dea295782f702f2689072d4fb33cb06c2263c
Subha Das <rohan6056@hotmail.com>
2021-06-25 17:27:32 -0700

    Added react front end

---
+++ front-end/src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {process.env.REACT_APP_CHOO_CHOO}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
