0fa733c5899946d7145a8bc113d594fbc74f78c9
Yash Totale <totaleyash@gmail.com>
2021-06-26 21:37:25 -0700

    Add PostgreSQL

---
+++ api/src/queries.ts
@@ -0,0 +1,73 @@
+import { RequestHandler } from "express";
+import pg from "pg";
+
+const Pool = pg.Pool;
+const pool = new Pool({
+  user: "me",
+  host: "localhost",
+  database: "api",
+  password: "password",
+  port: 5432,
+});
+
+export const getUsers: RequestHandler = (request, response) => {
+  pool.query("SELECT * FROM users ORDER BY id ASC", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+export const getUserById: RequestHandler = (request, response) => {
+  const id = parseInt(request.params.id);
+
+  pool.query("SELECT * FROM users WHERE id = $1", [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+export const createUser: RequestHandler = (request, response) => {
+  const { name, email } = request.body;
+
+  pool.query(
+    "INSERT INTO users (name, email) VALUES ($1, $2)",
+    [name, email],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(201).send(`User added with ID: ${results.rows[0].id}`);
+    }
+  );
+};
+
+export const updateUser: RequestHandler = (request, response) => {
+  const id = parseInt(request.params.id);
+  const { name, email } = request.body;
+
+  pool.query(
+    "UPDATE users SET name = $1, email = $2 WHERE id = $3",
+    [name, email, id],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).send(`User modified with ID: ${id}`);
+    }
+  );
+};
+
+export const deleteUser: RequestHandler = (request, response) => {
+  const id = parseInt(request.params.id);
+
+  pool.query("DELETE FROM users WHERE id = $1", [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).send(`User deleted with ID: ${id}`);
+  });
+};
--- api/start.ts
+++
@@ -1,4 +0,0 @@
-import express from "express";
-const app = express();
-
-app.listen(8080);
