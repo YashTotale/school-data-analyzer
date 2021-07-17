96b612f802880909b76805edea56744484977b27
Yash Totale <totaleyash@gmail.com>
2021-07-16 23:49:17 -0700

    Fix Get Districts & Get Schools Routes

--- api/v1/districts/routes/getDistricts.ts
+++ api/v1/districts/routes/getDistricts.ts
@@ -4,16 +4,16 @@ import { RequestHandler } from "express";
 // Internal Imports
 import District from "../model";
 
-interface Params {
+interface Query {
   /**
    * The limit of the query (defaults to 100)
    */
   limit?: string;
 }
 
-const getDistricts: RequestHandler<Params> = async (req, res) => {
-  const { limit: limitStr = "100" } = req.params;
-  const limit = parseInt(limitStr) || undefined;
+const getDistricts: RequestHandler<{}, any, any, Query> = async (req, res) => {
+  const { limit: limitStr = "100" } = req.query;
+  const limit = parseInt(limitStr) || 100;
 
   const districts = await District.findAll({
     limit,
--- api/v1/schools/routes/getSchools.ts
+++ api/v1/schools/routes/getSchools.ts
@@ -4,16 +4,16 @@ import { RequestHandler } from "express";
 // Internal Imports
 import School from "../model";
 
-interface Params {
+interface Query {
   /**
    * The limit of the query (defaults to 100)
    */
   limit?: string;
 }
 
-const getSchools: RequestHandler<Params> = async (req, res) => {
-  const { limit: limitStr = "100" } = req.params;
-  const limit = parseInt(limitStr) || undefined;
+const getSchools: RequestHandler<{}, any, any, Query> = async (req, res) => {
+  const { limit: limitStr = "100" } = req.query;
+  const limit = parseInt(limitStr) || 100;
 
   const schools = await School.findAll({
     limit,
