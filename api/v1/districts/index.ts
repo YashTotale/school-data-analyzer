96b3bc5eae6dca96149a7a95def8f2c6ed465d1a
Yash Totale <totaleyash@gmail.com>
2021-07-16 23:56:51 -0700

    Add Get District Route

--- api/v1/districts/index.ts
+++ api/v1/districts/index.ts
@@ -3,9 +3,11 @@ import express from "express";
 
 // Internal Imports
 import getDistricts from "./routes/getDistricts";
+import getDistrict from "./routes/getDistrict";
 
 const router = express.Router({ mergeParams: true });
 
 router.get("/", getDistricts);
+router.get("/:id", getDistrict);
 
 export default router;
---
+++ api/v1/districts/routes/getDistrict.ts
@@ -0,0 +1,34 @@
+// External Imports
+import { RequestHandler } from "express";
+
+// Internal Imports
+import District from "../model";
+
+const INVALID_ID = "Please provide a valid district id.";
+
+interface Params {
+  id: string;
+}
+
+const getDistrict: RequestHandler<Params> = async (req, res) => {
+  const { id } = req.params;
+  const index = parseInt(id);
+
+  if (typeof index !== "number") {
+    return res.status(400).send(INVALID_ID);
+  }
+
+  const district = await District.findOne({
+    where: {
+      index: id,
+    },
+  });
+
+  if (!district) {
+    return res.status(400).send(INVALID_ID);
+  }
+
+  return res.status(200).send(JSON.stringify(district));
+};
+
+export default getDistrict;
