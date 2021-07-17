3b02c4c9e4a76f9185c7ce35dac57b8462260c73
Yash Totale <totaleyash@gmail.com>
2021-07-16 18:43:21 -0700

    Set up Schools & Districts Routers

--- api/v1/index.ts
+++ api/v1/index.ts
@@ -1,6 +1,13 @@
 // External Imports
 import express from "express";
 
+// Internal Imports
+import schoolsRouter from "./schools";
+import districtsRouter from "./districts";
+
 const router = express.Router({ mergeParams: true });
+
+router.use("/schools", schoolsRouter);
+router.use("/districts", districtsRouter);
 
 export default router;
